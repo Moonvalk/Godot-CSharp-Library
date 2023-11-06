7c7e2030bfd9af3e0a447892573a0d07c6192c6f
Zack Harmon <elecgm@gmail.com>
2023-11-06 16:09:37 -0500

    Added basic texture animator and cursor controller to library.

--- Animation/Textures/TextureAnimation.cs
+++ Animation/Textures/TextureAnimation.cs
@@ -1,10 +1,26 @@
-
+using System.Collections.Generic;
 using Godot;
 using Moonvalk.Resources;
 
 namespace Moonvalk.Animation {
+	/// <summary>
+	/// Base class for animating textures found 
+	/// </summary>
 	[RegisteredType(nameof(TextureAnimation), "", nameof(Resource))]
 	public class TextureAnimation : Resource {
-		// ...
+		/// <summary>
+		/// The name of this animation.
+		/// </summary>
+		[Export] public string Name { get; protected set; }
+
+		/// <summary>
+		/// All textures that will be displayed in order.
+		/// </summary>
+		[Export] public List<Texture> Frames { get; protected set; }
+
+		/// <summary>
+		/// The duration of each frame in seconds.
+		/// </summary>
+		[Export] public List<float> FrameDurations { get; protected set; }
 	}
 }
